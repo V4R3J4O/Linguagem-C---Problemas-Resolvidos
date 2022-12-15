a7c52cd93bb79c3e7007eee88d0c5b138e03fbb7
V4R3J4O <ebenezermarquezine@gmail.com>
2022-12-15 12:35:33 +0000

    Introducao da Task.json e launch.json

---
+++ Struct/P3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <locale.h>
+
+typedef struct
+{
+    char nome [100];
+    int matricula;
+    char curso [50];
+}Aluno;
+
+int main (void)
+{
+    setlocale (LC_ALL, "Portuguese");
+    Aluno aluno[5];
+    
+
+    for (int i = 0; i < 2; i++)
+    {
+        printf("Digite o nome do aluno: ");
+        fgets(aluno[i].nome,100,stdin);
+        printf("Digite o nome do curso do aluno: ");
+        fgets(aluno[i].curso,50,stdin);
+        printf("Digite o numero da matricula do aluno: ");
+        scanf("%d",&aluno[i].matricula);
+        scanf("%c");
+        printf("\n\n");
+    }
+
+    
+    for (int i = 0; i < 2; i++)
+    {
+        printf("\n\n");
+        printf("Nome: %s",aluno[i].nome);
+        printf("Curso: %s",aluno[i].curso);
+        printf("Matr�cula: %d",aluno[i].matricula);
+        printf("\n\n");
+        
+    }
+    
+
+}
